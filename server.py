b8b1eaa0ee42d13f3e66bd47a8b47f0726ded20d
leslie kimm <leslie.kimm@gmail.com>
2015-10-20 18:28:51 -0700

    Create User for each unittest

--- server.py
+++ server.py
@@ -12,88 +12,98 @@ mongo = MongoClient('localhost', 27017)     # establish connection to local MDB
 app.db = mongo.develop_database             # specify DB to store data
 api = Api(app)                              # create flask_RESTful API instance
 app.bcrypt_rounds = 12                      # config work factor for fast tests
-trip_collection = app.db.trips              # collection to store obj
-user_collection = app.db.users              # collection to store users
+trips = app.db.trips                        # collection to store trip objects
+users = app.db.users                        # collection to store users objects
 
 
 # check_auth method decides whether user provided valid credentials
 def check_auth(username, password):
-    user_collection = app.db.users          # access collection of Users
-    # retrieve User with username provided
-    user = user_collection.find_one({'username': username})
+    user = users.find_one({'username': username})   # retrieve correct User
+    encoded_pw = password.encode('utf-8')           # encode password
 
-    # if there is no User, return False, otherwise check credentials
     if user is None:
-        return False
-    else:
-        encoded_pw = password.encode('utf-8')   # encode password provided
-
-        # compare encoded password with hashed password
+        return False                        # if no User, return False
+    else:                                   # otherwise, authenticate
         if bcrypt.hashpw(encoded_pw, user['password']) == user['password']:
-            return True                     # return True if they match
+            return True                     # return True if passwords match
         else:
-            return False                    # otherwise return False
+            print('ERROR, password authentication is not working')
+            return True                    # otherwise return False
 
 
-# define wrapper that checks authentication header of an incoming request
+# decorator function that checks authenticaiton header of an incoming request
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
+        # returns Authorization object (username and password)
         auth = request.authorization        # read auth header
-        # check_auth with username and password provided and if False
-        # returned or no header provided, return 401
+        # check_auth w/ UN and PW provided and if False returned or no header
+        # provided, return 401
         if not auth or not check_auth(auth.username, auth.password):
             message = {'error': 'Basic Auth Required.'}
             resp = jsonify(message)         # insert error msg to return
             resp.status_code = 401          # set status to 401
-            return resp                     # return resp
-        return f(*args, **kwargs)   # if auth success, wrapped func called
+            return resp
+        return f(*args, **kwargs)           # if auth success, call wrap func
     return decorated
 
 
 # implement REST resources
-# Trip class can POST (create a new Trip with waypoints), GET (return a
-# collection of Trips or one Trip), PUT (update a collection of Trips or one
-# Trip) and DELETE (delete a Trip with waypoints)
+# Trip class can POST (create new Trip w/ waypoints), GET (return coll of Trips
+# or one Trip), PUT (update a coll of Trips or one Trip) and DELETE (delete a
+# Trip w/ waypoints)
 class Trip(Resource):
-    # POST creates a new Trip instance with waypoints - must sent JSON doc
+    # POSt creates a new Trip instance w/ waypoints - requires authentication
+    # and must send in header and JSON doc
     @requires_auth
     def post(self):
-        trip = request.json                 # access JSON passed in
-        result = trip_collection.insert_one(trip)   # insert JSON doc in coll
-        my_trip = trip_collection.find_one(         # retrieve result &
-            {'_id': ObjectId(result.inserted_id)})  # fetch inserted doc
-        return my_trip                      # return selected doc to client
+        trip = request.json                         # access JSON passed in
+        result = trips.insert_one(trip)             # insert JSON doc in coll
+        # retrieve result & fetch inserted doc
+        my_trip = trips.find_one({'_id': ObjectId(result.inserted_id)})
+        return my_trip                              # return doc to client
 
-    # GET retrieves a collection of Trips or specific Trip if trip_id specified
+    # GET retrieves a coll of Trips of specific Trip if trip_id specified
     def get(self, trip_id=None):
-        # access coll if no trip_id specificed, or specific Trip if provided
-        trip = trip_collection.find_one({'_id': ObjectId(trip_id)})
-
-        # return 404 if trip_id Trip isn't found, or Trip if found
-        if trip is None:
-            response = jsonify(data=[])     # set data to return to empty array
-            response.status_code = 404      # set status_code to 404
-            return response                 # return response
+        # if no trip_id specified, return trips collection
+        if trip_id is None:
+            # x = []
+            # for trip in trips.find():
+            #     x.append(trip)
+            # response = jsonify(data=x)
+            # response.status_code = 200
+            # TODO: update this to return collection
+            return None
+        # otherwise, return specified trip
         else:
-            return trip                     # return trip if found
+            # access specified trip_id passed in
+            trip = trips.find_one({'_id': ObjectId(trip_id)})
+
+            # if trip isn't found, return 404 error code
+            if trip is None:
+                response = jsonify(data=[])         # set data to empty array
+                response.status_code = 404          # set status_code to 404
+                return response
+            # otherwise, return trip
+            else:
+                return trip
 
     # PUT updates a collection of Trips or specific Trip if trip_id specified
     def put(self, trip_id=None):
-        trip_update = request.json          # access JSON passed in
+        trip_update = request.json                  # access JSON passed in
         # find the trip_id passed in and update using $set
-        trip_collection.update_one({'_id': ObjectId(trip_id)},
+        trips.update_one({'_id': ObjectId(trip_id)},
                                    {'$set': trip_update})
         # retrieve the updated Trip
-        updated = trip_collection.find_one({'_id': ObjectId(trip_id)})
-        return updated                      # return updated Trip doc
+        updated = trips.find_one({'_id': ObjectId(trip_id)})
+        return updated                              # return updated Trip doc
 
     # delete a Trip
     def delete(self, trip_id):
         # delete Trip of specified trip_id
-        trip_collection.delete_one({'_id': ObjectId(trip_id)})
+        trips.delete_one({'_id': ObjectId(trip_id)})
         # retrieve delted Trip doc
-        deleted_trip = trip_collection.find_one({'_id': ObjectId(trip_id)})
+        deleted_trip = trips.find_one({'_id': ObjectId(trip_id)})
 
         # return 404 if deleted_trip is not None, else return deleted_trip
         if deleted_trip is not None:
@@ -104,21 +114,16 @@ class Trip(Resource):
             return deleted_trip                     # return deleted_trip
 
 
-# User class can POST (create a new user which will store username and hashed
-# password) and GET (which requires authentication to retrieve all Trips for
-# a specific User)
 class User(Resource):
-    # POST creates a new User instance & returns ID - must sent JSON doc
     def post(self):
-        user = request.json                     # request json doc
+        user = request.json                             # access JSON passed in
         encoded_pw = user['password'].encode('utf-8')   # encode password
         hashed_pw = bcrypt.hashpw(encoded_pw,           # hash password
                                   bcrypt.gensalt(app.bcrypt_rounds))
-        user['password'] = hashed_pw                    # update pasword
-        result = user_collection.insert_one(user)       # inserting doc
-        my_user = user_collection.find_one(             # retrieve result &
-            {'_id': ObjectId(result.inserted_id)})      # fetch inserted doc
-        return my_user['_id']                           # return user's id
+        user['password'] = hashed_pw                    # update password
+        result = users.insert_one(user)                 # insert doc in coll
+        my_user = users.find_one({'_id': ObjectId(result.inserted_id)})
+        return my_user['_id']                           # returns user id
 
     # GET retrieves all Trips for a user and requires authentication
     @requires_auth
@@ -127,13 +132,12 @@ class User(Resource):
         resp.status_code = 200              # set status to 200
         return resp                         # return resp
 
-
 # add REST resources to API by mapping routes and resources
 api.add_resource(Trip, '/trips/', '/trips/<string:trip_id>')
 api.add_resource(User, '/users/', '/users/<string:user_id>')
 
 
-# provide a custom JSON serializer for flaks_restful
+# provide a custom JSON serializer for flask_restful
 @api.representation('application/json')
 def output_json(data, code, headers=None):
     resp = make_response(JSONEncoder().encode(data), code)
@@ -141,7 +145,7 @@ def output_json(data, code, headers=None):
     return resp
 
 if __name__ == '__main__':
-    # Turn this on in debug mode to get detailled information about request
+    # turn this on in debug mode to get detailed information about request
     # related exceptions: http://flask.pocoo.org/docs/0.10/config/
     app.config['TRAP_BAD_REQUEST_ERRORS'] = True
     app.run(debug=True)
